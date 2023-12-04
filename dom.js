29d7d85ae7c327310a23dcf493594743d0d02b44
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-12-04 11:25:03 +0800

    Create a list-details manager

--- dom.js
+++ dom.js
@@ -3,10 +3,12 @@ export default function generateDom(){
     const createBtn = document.getElementById('create-list')
     const newListName = document.getElementById('list-name')
     const projectsContainer = document.getElementById('projects')
+    const main = document.getElementById('main')
     console.log("ISMAIL")
     createBtn.addEventListener('click', () => {
         tasksArray.push(newListName.value)
-        for (i = 0; i < tasksArray.length; i++){
+
+        for (let i = 0; i < tasksArray.length; i++){
             const project = document.createElement('div')
             project.classList.add(`project${i}`)
             const projectHeader = document.createElement('h1')
@@ -17,6 +19,25 @@ export default function generateDom(){
 
             project.appendChild(projectHeader)
             project.appendChild(deleteProject)
+            projectsContainer.appendChild(project)
         }
+        for (let i = 0; i < tasksArray.length; i++){
+            const card = document.createElement('div')
+            card.classList.add(`box${i}`)
+            card.classList.add('box')
+            const dltBtn = document.createElement('button')
+            dltBtn.innerText = 'X'
+            const para = document.createElement('p')
+            para.innerText = tasksArray[i]
+
+            card.appendChild(dltBtn)
+            card.appendChild(para)
+            main.appendChild(card)
+        }
+        tasksArray = []
+        newListName.value = ''
+
+        const newList = document.getElementById('new-list')
+        newList.style.display = 'none'
     })
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import generateDom from "./dom.js"
 const addBtn = document.getElementById('add-new-list')
 const newList = document.getElementById('new-list')
 addBtn.addEventListener('click', () => {
-    newList.style.display = 'none'
+    newList.style.display = 'flex'
 })
 
 generateDom()
